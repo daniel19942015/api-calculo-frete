2496796487de74706583e140459a968a81192a09
Daniel Silva <daniel199257@gmail.com>
2020-11-11 21:50:10 -0300

    Alterando porta

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const cors = require('cors')
 const { router }  = require("./src/routes")
-const port = 8080 || process.env.PORT;
+const port = process.env.PORT || 8080;
 
 app.use(express.json())
 app.use(cors())
