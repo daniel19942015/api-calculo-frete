c31ba9da9c4938d0bac650b07647ae2719b8e6a5
Daniel Silva <daniel199257@gmail.com>
2020-11-14 20:07:44 -0300

    Colocando resultado

--- src/Controller/GetValorFreteController.js
+++ src/Controller/GetValorFreteController.js
@@ -2,16 +2,17 @@ const { GetValor } = require("../Api/GetValor")
 
 async function GetValorFreteController(req, res){
 
-    console.log("POST -> Recebendo comando -> "+ req.body)
+    console.log("POST -> Recebendo comando -> "+ JSON.stringify(req.body))
     let result = await GetValor(req.body.cepOrigem, req.body.cepDestino);
     if(result){
-        console.log("Sucess POST - send data")
+        console.log("Success POST - send data -> " + JSON.stringify(result) + "\n")
         res.contentType("application/json");
         res.send(JSON.stringify(result))
     }else{
-        console.log("Error POST - send data")
+        let error = {status: 404, error: false}
+        console.log("Error POST - send data -> " + JSON.stringify(error) + "\n")
         res.contentType("application/json");
-        res.send(JSON.stringify({status: 404, error: false}))
+        res.send(JSON.stringify(error))
     }
 }
 
