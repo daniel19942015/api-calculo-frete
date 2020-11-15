0da914351d02352a0f0aea0c6e18d15d1b526936
Daniel Silva <daniel199257@gmail.com>
2020-11-14 22:00:19 -0300

    Colocando resultado

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const cors = require('cors')
 const { router }  = require("./src/routes")
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 const wokeDyno = require('woke-dyno')
 const uri = 'https://api-frete-calculo.herokuapp.com/'
 
